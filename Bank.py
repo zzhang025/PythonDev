2d263e2aa1f0f8c3b6d287f70525ae159b0e52d9
Zhixuan Zhang <zhangzx45@gmail.com>
2025-02-26 08:12:55 -0800

    debug

--- Bank.py
+++ Bank.py
@@ -48,7 +48,7 @@ class Transfer(Transaction):
         return f'Teller {self.get_teller_id()} transferred {self._amount} from account {self.get_customer_id()} to account {self._targetCustomerId}'
     
 class BankTeller:
-    def __int__(self, id):
+    def __init__(self, id):
         self._id = id
     
     def get_id(self):
@@ -87,7 +87,7 @@ class BankSystem:
         return self._transactions
 
     def open_account(self, customer_name, teller_id):
-        customerId = len(self._accounts())
+        customerId = len(self.get_accounts())
         account = BankAccount(customerId, customer_name, 0)
         self._accounts.append(account)
 
@@ -113,4 +113,103 @@ class BankSystem:
         transaction = Withdraw(customer_Id, teller_id, amount)
         self._transactions.append(transaction)
     
+import random 
+
+class BankBranch:
+    def __init__(self, address, cash_on_hand, bank_system):
+        self._address = address
+        self._cash_on_hand = cash_on_hand
+        self._bank_system = bank_system
+        self._tellers = []
     
+    def add_teller(self, teller):
+        self._tellers.append(teller)
+    
+    def _get_available_teller(self):
+        index = round(random.random() * (len(self._tellers) - 1))
+        return self._tellers[index].get_id()
+    
+    def open_account(self, customer_name):
+        if len(self._tellers) == 0:
+            raise Exception('No teller available')
+
+        teller_id = self._get_available_teller()
+        return self._bank_system.open_account(customer_name, teller_id)
+
+    def deposit(self, customer_id, amount):
+        if len(self._tellers) == 0:
+            raise Exception('No teller available')
+        teller_id = self._get_available_teller()
+        self._bank_system.deposit(customer_id, teller_id, amount)
+    
+    def withdraw(self, customer_id, amount):
+        if len(self._tellers) == 0:
+            raise Exception('No teller available')
+        
+        if amount > self._cash_on_hand:
+            raise Exception('Insufficient cash on hand')
+        
+        self._cash_on_hand -= amount
+        teller_id = self._get_available_teller()
+        self._bank_system.withdraw(customer_id, teller_id, amount)
+
+    def collect_cash(self, ratio):
+        cash_to_collect = round(self._cash_on_hand * ratio)
+        self._cash_on_hand -= cash_to_collect
+        return cash_to_collect
+
+    def provide_cash(self, amount):
+        self._cash_on_hand += amount
+
+class Bank:
+    def __init__(self, branches, bank_system, total_cash):
+        self._branches = branches
+        self._bank_system = bank_system
+        self._total_cash = total_cash
+    
+    def add_branch(self, address, initial_cash):
+        branch = BankBranch(address, initial_cash, self._bank_system)
+        self._branches.append(branch)
+        return branch
+    
+    def collect_cash(self, ratio):
+        for branch in self._branches:
+            self._total_cash += branch.collect_cash(ratio)
+    
+    def print_transactions(self):
+        for transaction in self._bank_system.get_tranasactions():
+            print(transaction.get_transaction_description())
+
+
+
+bankSystem = BankSystem([], [])
+bank = Bank([], bankSystem, 10000)
+
+branch1 = bank.add_branch('123 Main St', 1000)
+branch2 = bank.add_branch('456 Elm St', 1000)
+
+branch1.add_teller(BankTeller(1))
+branch1.add_teller(BankTeller(2))
+branch2.add_teller(BankTeller(3))
+branch2.add_teller(BankTeller(4))
+
+customerId1 = branch1.open_account('John Doe')
+customerId2 = branch1.open_account('Bob Smith')
+customerId3 = branch2.open_account('Jane Doe')
+
+branch1.deposit(customerId1, 100)
+branch1.deposit(customerId2, 200)
+branch2.deposit(customerId3, 300)
+
+branch1.withdraw(customerId1, 50)
+""" Possible Output:
+    Teller 1 opened account 0
+    Teller 2 opened account 1
+    Teller 3 opened account 2
+    Teller 1 deposited 100 to account 0
+    Teller 2 deposited 200 to account 1
+    Teller 4 deposited 300 to account 2
+    Teller 2 withdrew 50 from account 0
+"""
+
+bank.print_transactions()
